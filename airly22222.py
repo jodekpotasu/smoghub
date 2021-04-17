1584ed941892c9bf09b6e452ea9d487d076f1379
jodekpotasu <52979374+jodekpotasu@users.noreply.github.com>
2021-04-17 23:17:40 +0200

    naprawione i śmiga

--- airly22222.py
+++ airly22222.py
@@ -25,44 +25,10 @@ location = geolocator.geocode(address)
 print(location.address)
 print((location.latitude, location.longitude))
 
-# Before running the sample,
-#   you need to create .api_key file filled with your API key.
-# After you are logged in, you can copy it from here:
-#   https://developer.airly.eu/api
-try:
-    API_KEY = 'HC4lj4KL2cnNmKpLgANyYFdmTaa2GVAB'
-
-except FileNotFoundError:
-    print('Save your API key in samples/.api_key file', file=sys.stderr)
-    exit(1)
-
 LATITUDE = location.latitude
 LONGITUDE = location.longitude
 MAX_DIST_KM = 0.5
 
-
-async def main():
-    pyairly_logger = logging.getLogger('pyairly')
-    pyairly_logger.setLevel(logging.DEBUG)
-    pyairly_logger.addHandler(logging.StreamHandler(sys.stdout))
-    async with aiohttp.ClientSession() as http_session:
-        airly = Airly(API_KEY, http_session)
-
-        print('\nInstallations {:.2f} km apart from latitude {:f} and '
-              'longitude {:f}:\n'
-              .format(MAX_DIST_KM, LATITUDE, LONGITUDE))
-        sys.stdout.flush()
-        installations_list = await airly.load_installation_nearest(
-            LATITUDE, LONGITUDE, max_distance_km=MAX_DIST_KM, max_results=-1)
-        sys.stdout.flush()
-        for i in installations_list:
-            print("{}, {}.\n{}: {}\n".format(
-                i.address.displayAddress1, i.address.displayAddress2,
-                i.sponsor.description, i.sponsor.name))
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-# loop.close()
 
 import logging
 import sys
@@ -75,18 +41,11 @@ import aiohttp
 from airly import Airly
 from airly.measurements import Measurement
 
-# Before running the sample,
-#   you need to create .api_key file filled with your API key.
-# After you are logged in, you can copy it from here:
-#   https://developer.airly.eu/api
 try:
-    API_KEY = 'HC4lj4KL2cnNmKpLgANyYFdmTaa2GVAB'
+    API_KEY = 'nzvM1TYFA3NIR92r8MJ28fYWvbkJyCj1'
 except FileNotFoundError:
     print('Save your API key in samples/.api_key file', file=sys.stderr)
     exit(1)
-
-
-
 
 async def main():
     pyairly_logger = logging.getLogger('pyairly')
@@ -109,10 +68,24 @@ async def main():
             print("Measurements %s:" % description)
             for m in Measurement.MEASUREMENTS_TYPES:
                 print("%s: %s" % (m, current.values.get(m)))
-                weather_dict.append("%s: %s" % (m, current.values.get(m)))
+                weather_dict.append((m, current.values.get(m)))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 loop.close()
-
-print(weather_dict)
+PM1z = weather_dict[0]
+PM25z= weather_dict[1]
+PM10z= weather_dict[2]
+TEMPERATUREz= weather_dict[3]
+HUMIDITYz= weather_dict[4]
+PRESSUREz= weather_dict[5]
+pm25H= PM25z[1]
+pm10H= PM10z[1]
+temperature= TEMPERATUREz[1]
+humidity= HUMIDITYz[1]
+pressure= PRESSUREz[1]
+pm25H = float(pm25H)
+pm10H = float(pm10H)
+humidity = float(humidity)
+pressure = float(pressure)
+temperature = float(temperature)
